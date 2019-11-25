a546f7b14441bf4ce71d93213a8cb27347c43922
usuario <usuario@mbp-de-usuario.mynet>
2019-11-25 18:01:04 +0000

    techsatff package created, developer class created and done so far

---
+++ src/main/java/staff/techStaff/Developer.java
@@ -0,0 +1,9 @@
+package staff.techStaff;
+import staff.Employee;
+
+public class Developer extends Employee {
+
+    public Developer(String name, String NInumber, int salary){
+        super(name, NInumber, salary);
+    }
+}
